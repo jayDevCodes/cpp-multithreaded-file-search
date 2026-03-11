694066d5f17ea045ef68103c6d822cbb255c9ff0
7pratyush <pratyushgarg020@gmail.com>
2026-03-11 20:33:11 +0000

    feat: implement colored terminal output

--- search.cpp
+++ search.cpp
@@ -1,8 +1,15 @@
 #include "search.h"
 #include <iostream>
 #include <fstream>
+#include<string>
+#include <mutex>
 
 using namespace std;
+
+mutex coutMutex;
+
+const string BOLD_YELLOW = "\033[1;33m";
+const string RESET = "\033[0m";
 
 void searchInFile(const string& filename, const string& word) {
     ifstream file(filename);
@@ -12,8 +19,22 @@ void searchInFile(const string& filename, const string& word) {
     int lineNumber = 1;
 
     while (getline(file, line)) {
-        if (line.find(word) != string::npos) {
-            cout << filename << " : Line " << lineNumber << " -> " << line << endl;
+        size_t pos = line.find(word);
+        if (pos != string::npos) {
+            string highlightedLine = "";
+            size_t start = 0;
+
+            while ((pos = line.find(word, start)) != string::npos) {
+                highlightedLine += line.substr(start, pos - start); 
+                highlightedLine += BOLD_YELLOW + word + RESET;    
+                start = pos + word.length();                       
+            }
+            highlightedLine += line.substr(start); 
+
+            {
+                lock_guard<mutex> lock(coutMutex); 
+                cout << filename << " : Line " << lineNumber << " -> " << highlightedLine << endl;
+            }
         }
         lineNumber++;
     }
